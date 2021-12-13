fb807913f261c8018eb26e096486cb8803066131
nguyen  duy anh <nguyenduyanh@nguyens-MacBook-Pro.local>
2021-12-13 11:36:58 +0700

    add cnn

---
+++ skilledlab/cnn/experiment.py
@@ -0,0 +1,58 @@
+"""
+---
+title: Train a MLP on MNIST
+summary: >
+  Train a MLP on MNIST 
+---
+"""
+from typing import List, Optional
+
+from torch import nn
+
+from labml import experiment
+from labml.configs import option
+
+from skilledlab.experiments.mnist import MNISTConfigs
+from skilledlab.cnn import CnnNetBase
+
+class Configs(MNISTConfigs):
+    # Number of channels for each feature map size
+    n_channels: List[int] = [16, 16, 16, 16, 16]
+    # Kernel size of the initial convolution layer
+    first_kernel_size: int = 1
+
+
+@option(Configs.model)
+def _cnnnet(c: Configs):
+    """
+    ### Create model
+    """
+    base = CnnNetBase(c.n_channels,img_channels=1)
+    # Linear layer for classification
+    classification = nn.Linear(c.n_channels[-1], 10)
+    # Stack them
+    model = nn.Sequential(base, classification)
+    # Move the model to the device
+    return model.to(c.device)
+
+
+def main():
+    # Create experiment 
+    experiment.create(name='mlp',comment='mnist_cnn',writers={'screen'})
+    # Create configuration 
+    conf = Configs()
+    # Load configuration 
+    experiment.configs(conf, {
+        'optimizer.optimizer': 'Adam',
+        'optimizer.learning_rate': 0.001,
+        'epochs': 10
+    })
+    # Set model for saving/loading
+    experiment.add_pytorch_models({'model': conf.model})
+    # Start the experiment and run the training loop
+    with experiment.start():
+        conf.run()
+
+#
+if __name__ == '__main__':
+    main()
